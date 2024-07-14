9dbf14cf7dc78cf57833e22cf033a5262edaf1b9
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2024-07-14 12:51:04 +0700

    Test dev

--- atlanta-oss/src/App.js
+++ atlanta-oss/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Atlanta Dev
+          Atlanta Dev Test Environment
         </a>
       </header>
     </div>
