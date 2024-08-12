70543121cb2ab4dc89437755814db782045b87c7
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2024-08-12 12:51:51 +0700

    Implement navigationbar and authorization route

--- atlanta-oss/src/components/alert-page/Unauthenticated.js
+++ atlanta-oss/src/components/alert-page/Unauthenticated.js
@@ -8,6 +8,7 @@ const Unauthenticated = () => {
             <div className="container mt-5">
                 <h1>Unauthorized Access</h1>
                 <p>You are not allowed to access this page. Please contact the admin or operator for assistance.</p>
+                <p>Please login to access the website service <a href='/login'>Login to access the website</a></p>
             </div>
        </div>
     );
--- atlanta-oss/src/components/protectedRoute/protectedRoute.js
+++ atlanta-oss/src/components/protectedRoute/protectedRoute.js
@@ -1,10 +1,29 @@
+import { jwtDecode } from 'jwt-decode';
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, allowedRoles }) => {
     const isAuthenticated = localStorage.getItem('token') || sessionStorage.getItem('token');
 
-    return isAuthenticated ? children : <Navigate to="/unauthorized" />;
+    if (!isAuthenticated) {
+        return <Navigate to="/unauthorized" />;
+    }
+
+    try {
+        const decodedToken = jwtDecode(isAuthenticated);
+        const userRole = decodedToken.user_role;
+
+         // Check if the user's role is allowed to access this route
+        if(!allowedRoles.includes(userRole)) {
+            return <Navigate to="/unauthorized" />;
+        }
+
+        // If everything is okay, render the children components
+        return children;
+    } catch  (error) {
+         // If the token is invalid or cannot be decoded
+        return <Navigate to="/unauthorized" />;
+    }
 };
 
 export default ProtectedRoute;
